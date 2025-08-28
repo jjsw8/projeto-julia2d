578c6c9985cf0a383b705147c5ad142cbfd93f2b
julia_franciny <julia.franciny.oliveira@escola.pr.gov.br>
2025-08-28 10:50:41 -0300

    Add files via upload

---
+++ projeto-julia2d/conexão.php
@@ -0,0 +1,10 @@
+<?php
+
+//configuração do banco 
+$host = "localhost"; 
+$usuario = "root";
+$senha = "";
+$banco = "banco de dados";
+
+//conexão MySQL
+$conexao = mysql_connect ($host, $usuario, $senha, $banco);
